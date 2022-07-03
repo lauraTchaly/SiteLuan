80e2d65c7653332efad247330a919fc0ae6dd995
lauraTchaly <99758216+lauraTchaly@users.noreply.github.com>
2022-07-03 17:21:58 -0300

    M14
    Modificação da pasta MARKT.

--- markt/index.php
+++ markt/index.php
@@ -59,8 +59,58 @@ require($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
 
 <section>
 
-   <img src="/img/markt.jpeg.jpeg" alt="">
+<div class = "sobre-post">
+        <div class = "sobre-post_img">
+            <img src = "/img/market.jpeg">
+</div>
+<div class ="sobre-post_info">
+    <div class = "sobre-spot_date">
+          
+        </div>
+        <p class= "sobre-post_text">
+            <div class= "sobre-post_text">
+                <h1>MARKETING</h1>
+                <h3>Curiosidades sobre o marketing.</h3>
 
+<h5><p> Marketing digital são ações de comunicação que as empresas podem utilizar por meio da internet, da telefonia celular e outros meios digitais, para assim divulgar e comercializar seus produtos ou serviços, conquistando novos clientes e melhorando a sua rede de relacionamentos.</p></h5>
+
+
+
+<h3>Influência do marketing no dia a dia.</h3>
+
+<h5><p>A psicologia do marketing é utilizada na comunicação, ela a melhora, passando para um marketing mais particular, auxiliando no posicionamento pessoal das empresas em suas atuações, na percepção de produto e fidelização. O mundo das trocas que é o marketing almeja criar conexões com os clientes, indo além das empresas.</p></h5>
+
+
+
+<h3>Importância da sua eletiva pro meio humano.</h3>
+
+<h5><p>marketing pessoal, quando bem praticado, atua em prol do profissional, ajudando-o a construir a carreira que deseja. Ao valorizar as suas habilidades e os seus diferenciais, ele possibilita a conquista do primeiro emprego, ganhar um aumento ou receber aquela promoção tão desejada.</p></h5>
+
+
+
+<h3>Áreas de trabalho.</h3>
+
+<h5><p><ol> 1. Inbound Marketing</ol></p>
+
+<p><ol> 2. Estratégia de SEO</ol></p>
+
+<p><ol> 3. Mídias Sociais</ol></p>
+
+<p><ol> 4. Tráfego pago</ol></p>
+
+<p><ol> 5. Neuromarketing</ol></p>
+
+<p><ol> 6. Gestor de e-commerce</ol></p>
+
+<p><ol> 7. Produção de conteúdo</ol></p>
+
+<p><ol> 8. Marketing de Relacionamento</ol></p>
+
+<p><ol> 9. Endomarketing</ol></p></h5>
+                </p>
+    </div>
+</div>
+</div>      
 </section>
 
 <aside>
